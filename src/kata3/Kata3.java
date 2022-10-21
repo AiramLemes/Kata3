434233719831fac78fe9e357c9b1b549e4550e47
AiramLemes <90828450+AiramLemes@users.noreply.github.com>
2022-10-21 13:32:58 +0100

    Kata3 Versión 1

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,18 @@
+
+package kata3;
+
+/**
+ *
+ * @author airam
+ */
+public class Kata3 {
+
+    
+    public static void main(String[] args) {
+        
+        HistogramDisplay histograma = new HistogramDisplay();
+        histograma.execute();
+        
+    }
+    
+}

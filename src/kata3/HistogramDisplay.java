86357d386ddce89a67c15e36152a0c2fa1764c01
AiramLemes <90828450+AiramLemes@users.noreply.github.com>
2022-10-21 13:37:32 +0100

    Kata3 Versión 2

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -2,60 +2,70 @@
 package kata3;
 
 import java.awt.Dimension;
- import javax.swing.JPanel;
- import org.jfree.chart.ChartFactory;
- import org.jfree.ui.ApplicationFrame;
- import org.jfree.chart.ChartPanel;
- import org.jfree.chart.JFreeChart;
- import org.jfree.chart.plot.PlotOrientation;
- import org.jfree.data.category.DefaultCategoryDataset;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 
- public class HistogramDisplay extends ApplicationFrame {
-
-     public HistogramDisplay() {
-         super("HISTOGRAMA");
-
-         setContentPane(createPanel());
-         pack();             // Hace que la ventana sea lo más pequeña posible
-     }
-
-
-     public void execute() {
-
-         setVisible(true);
-
-     }
-
-
-     private JPanel createPanel() {
-
-         ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
-         setPreferredSize(new Dimension(500,400));
-
-
-         return chartPanel;
-     }
-
-
-     private JFreeChart createChart(DefaultCategoryDataset dataSet) {
-
-         JFreeChart chart = ChartFactory.createBarChart("Histograma JFreeChart", 
-                 "Dominios email", "Nº de emails", createDataset(), PlotOrientation.VERTICAL, 
-                 false, false, rootPaneCheckingEnabled);
-
-         return chart;
-     }
-
-     private DefaultCategoryDataset createDataset() {
-
-         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-
-         dataSet.addValue(5, " ", "gmail.com");
-         dataSet.addValue(10, " ", "ulpgc.es");
-         dataSet.addValue(7, " ", "ull.es");
-         dataSet.addValue(2, " ", "hotmail.es");
-
-         return dataSet;
-     }
- }
+public class HistogramDisplay extends ApplicationFrame {
+    
+    
+    private final Histogram<String> histogram;
+    
+    
+    public HistogramDisplay(Histogram<String> histogram) {
+        super("HISTOGRAMA");
+        
+        this.histogram = histogram;
+        
+        setContentPane(createPanel());
+        pack();             // Hace que la ventana sea lo más pequeña posible
+    }
+    
+    
+    
+    public void execute() {
+        
+        setVisible(true);
+        
+    }
+    
+    
+    
+    private JPanel createPanel() {
+        
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        setPreferredSize(new Dimension(500,400));
+        
+        
+        return chartPanel;
+    }
+    
+    
+    
+    private JFreeChart createChart(DefaultCategoryDataset dataSet) {
+        
+        JFreeChart chart = ChartFactory.createBarChart("Histograma JFreeChart", 
+                "Dominios email", "Nº de emails", createDataset(), PlotOrientation.VERTICAL, 
+                false, false, rootPaneCheckingEnabled);
+        
+        return chart;
+    }
+    
+    
+    private DefaultCategoryDataset createDataset() {
+        
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        
+        
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), " ", key);
+        }
+        
+        return dataSet;
+    }
+}
--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -10,8 +10,26 @@ public class Kata3 {
     
     public static void main(String[] args) {
         
-        HistogramDisplay histograma = new HistogramDisplay();
-        histograma.execute();
+        Histogram<String> histogram = new Histogram();
+        
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("gmail.es");
+        histogram.increment("gmail.es");
+        histogram.increment("gmail.es");
+        histogram.increment("gmail.es");
+        histogram.increment("outlook.es");
+        histogram.increment("outlook.es");
+        histogram.increment("outlook.com");
+        histogram.increment("outlook.com");
+        histogram.increment("outlook.com");
+        
+        new HistogramDisplay(histogram).execute();
         
     }
     
